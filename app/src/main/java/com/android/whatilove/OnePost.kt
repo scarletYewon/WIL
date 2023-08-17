810f258504942a615da04d0a6e01b37de5410391
Lee do yeon <130144220+byu-rin@users.noreply.github.com>
2023-08-17 13:55:05 +0900

    Add files via upload

---
+++ app/src/main/java/com/android/whatilove/OnePost.kt
@@ -0,0 +1,14 @@
+package com.android.whatilove
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class OnePost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onepost)
+
+
+    }
+}

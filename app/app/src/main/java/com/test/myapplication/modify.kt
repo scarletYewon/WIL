71ecb9442fd19714832940fc3179233b382ed934
이예원 <81704418+scarletYewon@users.noreply.github.com>
2023-08-20 13:36:15 +0900

    .

--- app/app/src/main/java/com/test/myapplication/modify.kt
+++ app/app/src/main/java/com/test/myapplication/modify.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
